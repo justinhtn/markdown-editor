1aaa17bde817a1d67932ace1ee54241129fb8c3c
justinhtn <justinhoughton@fastmail.com>
2020-08-04 15:19:55 -0700

    includes build folder

---
+++ docs/precache-manifest.0d49f153fbbcfe9b39589b6bd04c3f08.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62b5119883bceb4218e1633411d0c752",
+    "url": "/markdown-editor/index.html"
+  },
+  {
+    "revision": "7344dc22fcc7d936b790",
+    "url": "/markdown-editor/static/css/main.9fd7d4d3.chunk.css"
+  },
+  {
+    "revision": "e367e95354931ecb63fb",
+    "url": "/markdown-editor/static/js/2.5a64c42e.chunk.js"
+  },
+  {
+    "revision": "7344dc22fcc7d936b790",
+    "url": "/markdown-editor/static/js/main.d93b3764.chunk.js"
+  },
+  {
+    "revision": "323a14285d6b26e1c229",
+    "url": "/markdown-editor/static/js/runtime~main.9746201c.js"
+  }
+]);
